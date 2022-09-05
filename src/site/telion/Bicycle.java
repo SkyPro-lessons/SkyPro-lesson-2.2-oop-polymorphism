1ab11042b905cd4f7fc7998503d1d6d766f11ff4
Telion <www.neon@mail.ru>
2022-09-05 22:38:00 +0500

    Начальный код программы, который необходимо исправить в соответствии с ООП

---
+++ src/site/telion/Bicycle.java
@@ -0,0 +1,10 @@
+package site.telion;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
